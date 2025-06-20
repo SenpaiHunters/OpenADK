2e9ebafb62ca7b177a1df4b105634d45ea0c37cb
Kami <kamibusinessinquirers@gmail.com>
2025-06-20 22:19:49 +1000

    🐞 Fix build bugs

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "OpenADK",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v14),
     ],
     products: [
         .library(
@@ -13,15 +13,27 @@ let package = Package(
             targets: ["OpenADK"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-numerics.git", from: "1.0.0"),
+    ],
     targets: [
         .target(
             name: "OpenADKObjC",
+            dependencies: [
+                .product(name: "Algorithms", package: "swift-algorithms"),
+                .product(name: "Numerics", package: "swift-numerics"),
+            ],
             path: "Sources/OpenADKObjC",
-            publicHeadersPath: "."
+            publicHeadersPath: ".",
         ),
         .target(
             name: "OpenADK",
-            dependencies: ["OpenADKObjC"],
+            dependencies: [
+                "OpenADKObjC",
+                .product(name: "Algorithms", package: "swift-algorithms"),
+                .product(name: "Numerics", package: "swift-numerics"),
+            ],
             path: "Sources/OpenADK"
         ),
         .testTarget(
